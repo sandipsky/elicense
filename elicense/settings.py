6202217f4c8e95a60ded584e34030f5938fbc1b0
sandipsky <sandipshakya75@gmail.com>
2022-08-09 10:18:37 +0545

    created models

--- elicense/settings.py
+++ elicense/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'license',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'elicense.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/static/'
+
+STATICFILES_DIRS = [
+	os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- license/models.py
+++ license/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name 
+
+class ApplicantInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=200)
+    midname = models.CharField(max_length=200)
+    lastname = models.CharField(max_length=200)
+    dob = models.DateField()
+    phone = models.CharField(max_length=10)
+    father = models.CharField(max_length=200)
+    mother = models.CharField(max_length=200)
+    blood = models.CharField(max_length=3)
+    citizenshipno = models.CharField(max_length=50)
+    citizenshipdate = models.DateField()
+    citizenshipdistrict = models.CharField(max_length=200)
+    citizenship = models.ImageField()
+    zone = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    ward = models.CharField(max_length=3)
+    tole = models.CharField(max_length=200)
+
+class License(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_approved = models.BooleanField(default=False)
+    applicant = models.ForeignKey(ApplicantInfo, on_delete=models.CASCADE)
+    
